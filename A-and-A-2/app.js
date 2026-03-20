d90fbad0122e06b74bcd45586de318a7d59a9c79
piyush saroj <piyushsaroj222006@gmail.com>
2026-03-20 16:13:49 +0530

    helloji

--- A-and-A-2/app.js
+++ A-and-A-2/app.js
@@ -1,8 +1,60 @@
 const express = require('express');
 const app = express();
+const userModel = require("./models/user");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-app.get("/" ,function (req,res){
-    res.send("hello")
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(cookieParser());
+
+app.get('/' ,function (req,res){
+    res.render("index");
 })
 
+app.post('/create', function (req,res){
+    let {username, email, password, age} = req.body;
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+
+            let createdUser = await userModel.create({
+                username,
+                email,
+                password: hash,
+                age
+            });
+
+            let token = jwt.sign({email}, "hello");
+            res.cookie("token", token);
+        
+            res.send(createdUser);
+        })
+    })
+
+});
+
 app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
