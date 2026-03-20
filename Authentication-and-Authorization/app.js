2af1b0edeb7a1b499fefd3cb214ffcdac27f0a81
piyush saroj <piyushsaroj222006@gmail.com>
2026-03-20 13:49:38 +0530

    polololoo

--- Authentication-and-Authorization/app.js
+++ Authentication-and-Authorization/app.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
 
 app.get("/", function (req, res) {
-    bcrypt.compare("piyush", "$2b$10$5jDqQ0guL9DrxexrhzmeQulBS8UkIlrIgmVng9S0DUdYKVT2pHEgS", function(err, result) {
-        console.log(result)
-    // result == true
-});
+    let token = jwt.sign({email:"piyush@zohomail.in"}, "secret" );
+    res.cookie("token", token);
+    res.send("done")
+})
+
+app.get("/read", function (req, res) {
+    let data = jwt.verify(req.cookies.token, "secret")
+    res.send(data);
 })
 
 app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
